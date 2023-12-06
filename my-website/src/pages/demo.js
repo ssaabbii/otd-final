7a6a413276c50b367d750e60a39d4ef8c263b8fb
ssaabbii <nurdolotovasabina02@mail.ru>
2023-12-06 21:02:25 +0700

    demo (but inverted img)

--- my-website/src/pages/demo.js
+++ my-website/src/pages/demo.js
@@ -1,91 +1,63 @@
+import React, { useState } from 'react';
 import clsx from 'clsx';
 import Link from '@docusaurus/Link';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import Layout from '@theme/Layout';
-import HomepageFeatures from '@site/src/components/HomepageFeatures';
-
 import Heading from '@theme/Heading';
 import styles from './index.module.css';
-
 import SwaggerUI from "swagger-ui-react";
 import "swagger-ui-react/swagger-ui.css";
 import "./api.css";
 
+function Image() {
+  const [invertedImage, setInvertedImage] = useState(null);
 
-function DEMO() {
-    const {siteConfig} = useDocusaurusContext();
-    return (
-        <div className="container">
-            <p>Demo</p>
-        </div>
-    );
-}
+  const handleImageUpload = (file) => {
+    const formData = new FormData();
+    formData.append('image', file);
 
+    fetch('http://localhost:8000/invert-image', {
+      method: 'POST',
+      body: formData
+    })
+    .then(response => response.blob())
+    .then(blob => {
+      setInvertedImage(URL.createObjectURL(blob));
+    })
+    .catch(error => {
+      console.error('Error uploading image:', error);
+    });
+  };
+
+  const {siteConfig} = useDocusaurusContext();
+
+  return (
+    <header className={clsx('hero hero--primary', styles.heroBanner)}>
+      <div className="container">
+        <Heading as="h2" className="hero__title">
+          Загрузите фото и получите предсказание 
+        </Heading>
+        <p className="hero__subtitle">Модель определит какие проблемы с кожей у Вас есть</p>
+        <input type="file" onChange={(e) => handleImageUpload(e.target.files[0])} />
+        {invertedImage && <img src={invertedImage} alt="Inverted" className={styles.invertedImage} />}
+      </div>
+    </header>
+  );
+};
 
 export default function Home() {
-    const {siteConfig} = useDocusaurusContext();
-    return (
-        <Layout>
-            <DEMO/>
-        </Layout>
-    )
+  const {siteConfig} = useDocusaurusContext();
 
+  return (
+    <Layout>
+      <div className="container">
+        <Image />
+       
+      </div>
+    </Layout>
+  )
 }
-// import React, { useState } from 'react';
-// import axios from 'axios';
 
-// function App() {
-//   const [file, setFile] = useState(null);
-//   const [classes, setClasses] = useState([]);
-  
-  
-//   const handleFileChange = (event) => {
-//     setFile(event.target.files[0]);
-//   };
 
-//   const handleSubmit = (event) => {
-//     event.preventDefault();
-//     const formData = new FormData();
-//     formData.append('file', file);
-   
-//     axios
-//     .post('http://0.0.0.0:8000/detection', formData ,
-//       {headers: {
-//         'Access-Control-Allow-Credentials': true
-//       }
-//     })
-    
-//       .then((response) => {
-//         console.log(response.data)
-//         setClasses(response.data.classes);
-//       })
-//       .catch((error) => {
-//         console.error(error);
-//       });
-//   };
 
-//   return (
-//     <div>
-//       <form onSubmit={handleSubmit}>
-//         <input type="file" onChange={handleFileChange} />
-//         <button type="submit">Detect Objects</button>
-//       </form>
-    
-      
-//         {classes.map((classa,index) =>(
-        
-//           <div key={index}>
-//             <p>class: {classa}</p>
-      
-      
-// </div>
-//     ) ) } 
-
-    
-   
-  
-//   </div>);
-// }
-
-// export default App;
 
